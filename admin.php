46095958e288922de0f8b25ae54a21aafd745f15
xsine <neoway@126.com>
2021-11-29 02:51:27 +0800

    init

--- admin.php
+++ admin.php
@@ -111,7 +111,7 @@ $html=<<<EOT
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">管理员</a>
+          <a class="navbar-brand" href="#">您好，管理员</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav navbar-right">
--- check.php
+++ check.php
@@ -14,7 +14,6 @@ function checkUser(){
         $pasw=str_replace(' ','+',$pasw);
         $deuser=RsaUtils::privateDecrypt($user,$GLOBALS['privatekey']);
         $depasw=RsaUtils::privateDecrypt($pasw,$GLOBALS['privatekey']);
-        $depasw=str_replace(' ','+',$depasw);
         $sql="select password from users where username='$deuser'";
         $redata=$mysqli->query($sql);
         $row=$redata->fetch_array();
@@ -43,6 +42,49 @@ function checkUser(){
         exit("未登录");
     }
 }
+
+function getUserName(){
+    $db_user=$GLOBALS['sqlUser'];
+    $db_pass=$GLOBALS['sqlPass'];
+    $db_host=$GLOBALS['sqlHost'];
+    $db_database=$GLOBALS['sqlDatabase'];
+    $mysqli=new mysqli($db_host,$db_user,$db_pass,$db_database);
+    if(isset($_COOKIE["username"])&&isset($_COOKIE["password"])){
+        $user=$_COOKIE["username"];
+        $pasw=$_COOKIE["password"];
+        $user=str_replace(' ','+',$user);
+        $deuser=RsaUtils::privateDecrypt($user,$GLOBALS['privatekey']);
+
+        $sql="select password from users where username='$deuser'";
+        $redata=$mysqli->query($sql);
+        $row=$redata->fetch_array();
+        if($redata->num_rows<1)
+        {   setcookie("username","");
+            setcookie("password","");
+            header("Location:index.php");
+            exit();
+        }
+        if ($row[0]==$depasw)
+        {
+            $sql="select limits from users where username='$deuser'";
+            $redata=$mysqli->query($sql);
+            $row=$redata->fetch_array();
+            return $row[0];
+        }else{
+            setcookie("username","");
+            setcookie("password","");
+            header("Location:index.php");
+            exit();
+        }
+    }else{
+        setcookie("username","");
+        setcookie("password","");
+        header("Location:index.php");
+        exit("未登录");
+    }
+}
+
+
 function checkUserFromString($user,$pasw){
     $db_user=$GLOBALS['sqlUser'];
     $db_pass=$GLOBALS['sqlPass'];
