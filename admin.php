9c49abcafd832c7bb526130ffbdad2a79ee46309
xsine <neoway@126.com>
2021-12-07 15:26:34 +0800

    init

--- admin.php
+++ admin.php
@@ -180,10 +180,7 @@ $html=<<<EOT
          
         </div>
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-       
            $context
-       
-        
         </div>
       </div>
     </div>
