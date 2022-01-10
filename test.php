61e365dad13e75c188b2b6af4570429e1c6b5aaa
renzibin <by891666805@163.com>
2022-01-10 13:25:51 +0800

    init

--- test.php
+++ test.php
@@ -1,28 +1,20 @@
 <?php
+require "config.php";
+$a=array('title' => '老师','list'=>array(array("仓库",0,1,2),array("对外",7)));
+//$b=array(1=>'a',3=>array('b','c'));
 
-#$a=array('title' => '老师','list'=>array(array("仓库",0,1,2),array("对外",7)));
-$b=array(1=>'a',3=>array('b','c'));
-
-if (array_key_exists(3,$b))
-//$enc = json_encode($a);
-echo 'ok';
+//if (array_key_exists(3,$b))
+$enc = json_encode($a);
+echo $enc;
 
 
 
-$sql="select * from depot1";
+$sql="update place_a set place='1' where id='$enc'";
 $db_host=$GLOBALS['sqlHost'];
 $db_user=$GLOBALS['sqlUser'];
 $db_psw=$GLOBALS['sqlPass'];
 $db_name=$GLOBALS['sqlDatabase'];
 $mysqli=new mysqli($db_host,$db_user,$db_psw,$db_name);
 $res=  $mysqli->query($sql);
-$tr="";
-if ($res)
-    if($res->num_rows>0)
-        while ($row=$res->fetch_array()){
-            $tr=$tr."<tr>";
-            $tr=$tr."<td>".$row[0]."</td>";
-            $tr=$tr."<td>".$row[3]."</td>";
-            $tr=$tr."<td>".$row[4]."</td>";
-            $tr=$tr."</tr>";
-        }
+
+
